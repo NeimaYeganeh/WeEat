d604a82fabd1d1ed757f0f7987f75e9c3b2f34f3
Jackie Krone <jackie.marie.krone@gmail.com>
2019-05-23 16:16:55 -0700

    added authentication email/pass

--- WeEat/login.js
+++ WeEat/login.js
@@ -1,61 +1,55 @@
-
-//var firebase = require("firebase");
-
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-//import * as firebase from "firebase/app";
-
-// Add the Firebase services that you want to use
-//import "firebase/auth";
-//import "firebase/firestore";
-
-//function login(){alert("login function successfully called")};
-
-
-
-function login(){
-
-alert("login function successfully called");
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-//const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-//const admin = require('firebase-admin');
-//admin.initializeApp();
-
-
-
-    // Get authentication elements
-    const userEmail = document.getElementById("user");
-    const pass = document.getElementById("pass");
-    const btnLogin = document.getElementById("Login");
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user)
+    {
+        document.getElementById("user_div").style.display = "block";
+        document.getElementById("btnLogin").style.display = "none";
+    }
     
-    window.alert(userEmail+"  "+pass);
+    var user = firebase.auth().currentUser; 
+   
+    if(user != null)
+    {
+        var email_id = user.email;
+        document.getElementById("user_para").innerHTML = email_id;
+        
+        //document.getElementById("loginModal").style.display.innerHTML = "Logout";
+    }
+    else 
+    {
+        document.getElementById("user_div").style.display = "none";
+        document.getElementById("btnLogin").style.display = "block";
+    }
 
-    // Add login event
-    btnLogin.addEventListener('click', e=>
-        {
-            // Get email and pass
-            const email = userEmail.value;
-            const pass = pass.value;
-            const auth = firebase.auth();
+});
 
-            // Sign in
-            const promise = auth.signInWithEmailAndPass(email,pass);
-            promise.catch(e => console.log(e.message));
-        });
 
-    // Add a realtime listener to keep checking authentication
-    firebase.auth().onAuthStateChanged(firebaseUser =>
-        {
-            if(firebaseUser)
-            {
-                console.log(firebaseUser);
-            }
-            else
-            {
-                console.log('not logged in');
-            }
-        });
+function logout()
+{
+    firebase.auth().signOut(); 
 }
+
+
+function login()
+{
+  var userEmail = document.getElementById("user").value;
+  var userPass = document.getElementById("pass").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+
+
+
--- index.js
+++ index.js
@@ -3,6 +3,17 @@ exports.addTwoNumbers = function (a, b) {
     return 0;
 }
 
+function login(){
+var firebase = require("firebase");
+//const firebase = require("firebase/app"); 
+const auth = require("firebase/auth");
+const database = require("firebase/database");
+const firestore = require("firebase/firestore");
+
+
+alert("login function successfully called");
+}
+
 /*
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
