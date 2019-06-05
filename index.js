dc54e633aa5a1f1e2f0414f24a6ce2545f746e81
Jackie Krone <jackie.marie.krone@gmail.com>
2019-06-04 18:29:55 -0700

    removed unused code

--- index.js
+++ index.js
@@ -3,13 +3,3 @@ exports.addTwoNumbers = function (a, b) {
     return 0;
 }
 
-function login(){
-var firebase = require("firebase");
-//const firebase = require("firebase/app"); 
-const auth = require("firebase/auth");
-const database = require("firebase/database");
-const firestore = require("firebase/firestore");
-
-
-alert("login function successfully called");
-}
