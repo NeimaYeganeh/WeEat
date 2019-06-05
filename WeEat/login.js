93cfff434913f4052da67fa4eb904ec72f4d89ec
Steven Sun <hsun03@calpoly.edu>
2019-06-04 18:42:05 -0700

    login, logout and adminPannel
    added logout functionality as well as link to admin pannel

--- WeEat/login.js
+++ WeEat/login.js
@@ -2,7 +2,16 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user)
     {
         document.getElementById("user_div").style.display = "block";
-        document.getElementById("btnLogin").style.display = "none";
+        document.getElementById("loginModal").style.display = "none";
+        var nav_id=document.getElementById("homepage");
+        var anchor=nav_id.getElementsByTagName('a');
+        var aTag = document.createElement('a');
+        aTag.innerHTML = "Admin Pannel";
+        anchor[2].innerHTML="Logout";
+        anchor[2].removeAttribute("href");
+        anchor[2].setAttribute('onclick','logout()');
+        nav_id.insertBefore(aTag,anchor[2]);
+        //anchor[2].onclick="logout()";
     }
     
     var user = firebase.auth().currentUser; 
@@ -17,16 +26,16 @@ firebase.auth().onAuthStateChanged(function(user) {
     else 
     {
         document.getElementById("user_div").style.display = "none";
-        document.getElementById("btnLogin").style.display = "block";
+        document.getElementById("map").style.display = "block";
+        var nav_id=document.getElementById("homepage");
+        var anchor=nav_id.getElementsByTagName('a');
+        anchor[2].innerHTML="Login";
+        anchor[2].setAttribute('href','#login');
+        anchor[2].setAttribute('onclick','document.getElementById("loginModal").style.display="block"');
+        anchor[3].remove();
     }
 
 });
-
-
-function logout()
-{
-    firebase.auth().signOut(); 
-}
 
 
 function login()
