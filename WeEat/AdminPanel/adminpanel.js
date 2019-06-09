1ec4bed401a2108d627b15ce954b04f6f180ce54
neimayeganeh <nyeganeh@calpoly.edu>
2019-06-08 18:40:35 -0700

    Displays pins from DB and displays a div with pin info when you click on a pin row. Still needs formatting/css but functionality is solid

--- WeEat/AdminPanel/adminpanel.js
+++ WeEat/AdminPanel/adminpanel.js
@@ -1,63 +1,99 @@
 //var dt = require('datatables.net')();
 
 $(document).ready(() => {
-    renderPinTable();
+    getPins(true);
 });
 
-renderPinTable = () => {
-    let pins = [
-        {
-            num: 132,
-            name: 'Neim',
-        },
-        {
-            num: 34,
-            name: 'Nat',
-        },
-        {
-            num: 20,
-            name: 'Asher',
-        },
-        {
-            num: 4,
-            name: 'Larkin',
-        },
-        {
-            num: 1234,
-            name: 'Yeg',
-        },
-        {
-            num: 4321,
-            name: 'Megan',
-        },
-    ];
-    let format = '<"fg-toolbar ui-toolbar ui-widget-header ui-helper-clearfix ui-corner-tl ui-corner-tr"lfr>'
-                + 't'
-                + '<"fg-toolbar ui-toolbar ui-widget-header ui-helper-clearfix ui-corner-bl ui-corner-br"ip>';
+getPins = (isInit) => {
+    let db = firebase.firestore();
+    db.collection("Pins").onSnapshot((querySnapshot) => {
+        let pins = [];
+        querySnapshot.forEach((doc) => {
+            let pin = doc.data();
+            if (!pins.includes(pin)) {
+                pins.push(pin);
+            }
+        });
+        if (isInit) {
+            renderPinTable(pins);
+            isInit = false;
+        }
+        else {
+            refreshPinTable(pins);
+        }
+    });
+}
 
+renderPinTable = (pins) => {
+    console.log(pins);
     let datatable = $('#pinTable').DataTable({
         data: pins,
-        format: format,
-        lengthMenu: [[5, 10, -1], [5,10,'All']],
+        lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'All']],
+        createdRow: (row, data, index) => {
+            renderHTMLDataset($(row)[0], data);
+        },
         columns: [
             {
-                title: 'Number',
-                data: 'num',
-//                render: (a,b,c) => {
-//                    console.log(a,b,c);
-//                    return c;
-//                }
+                title: 'Title',
+                data: 'title',
             },
             {
-                title: 'Name',
-                data: 'name',
+                title: 'Time',
+                data: 'time',
+                orderable: false,
+            },
+            {
+                title: 'Sponsor',
+                data: 'sponsor',
             },
         ],
     });
-
-    datatable.draw();
-    datatable.on('draw', () => {
-        console.log('drawing!!');
-    });
-    console.log(pins, datatable);
+    setRowListeners();
 }
+
+renderHTMLDataset = (row, data) => {
+    row.dataset.title = data.title || '';
+    row.dataset.time = data.time || '';
+    row.dataset.sponsor = data.sponsor || '';
+    row.dataset.location = data.location || '';
+    row.dataset.lat = data.coordinates[1] || '';
+    row.dataset.long = data.coordinates[0] || '';
+    row.dataset.contact = data.contact || '';
+}
+
+refreshPinTable = (pins) => {
+    console.log(pins);
+    let datatable = $('#pinTable').DataTable();
+    datatable.clear().draw();
+    datatable.rows.add(pins);
+    datatable.columns.adjust().draw();
+    setRowListeners();
+}
+
+setRowListeners = () => {
+    $('#pinTable tbody').on('click', 'tr', (event) => {
+        let rowData = event.currentTarget.dataset;
+        createPinInfoSection(rowData);
+    });
+}
+
+createPinInfoSection = (pinData) => {
+    console.log(pinData);
+//    let title = document.createElement('div');
+//    let time = document.createElement('div');
+//    let sponsor = document.createElement('div');
+//    let location = document.createElement('div');
+//    let lat = document.createElement('div');
+//    let long = document.createElement('div');
+//    let contact = document.createElement('div');
+    
+    let container = document.createElement('div');
+    container.innerHTML = `${pinData.title} ${pinData.time} ${pinData.sponsor} ${pinData.location} ${pinData.lat} ${pinData.long} ${pinData.contact}`;
+    
+    let pinInfoDiv = $('#pinInfoDiv');
+    
+    pinInfoDiv.empty().append(container.outerHTML);
+}
+
+
+
